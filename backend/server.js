e5c99b621609cf04667079969f0eedb43cf6f770
Assinix <assinix@email.com>
2026-03-22 15:12:43 -0700

    Atualizado server.js e frontend/index.html com novas alterações do formulário

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-console.log('ADMIN PASSWORD:', process.env.ADMIN_PASSWORD);
 
 const app = express();
 
